ee5ba8a69c5729f5bbc65d0b2c76a2e730b83fc1
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-02-01 13:01:40 +0100

    updated

--- main.js
+++ main.js
@@ -1,9 +1,56 @@
 /**
  * The file where everything comes together
  */
-import * as todoList from "./scripts/todoList.js";
-import { createTodoElement } from "./scripts/createTodoElement.js";
+import { addTask, getAllTodoes } from "./scripts/todoList.js";
+import { createNewTask } from "./scripts/createTodoElement.js";
 
 // What elements do we need?
 // What events do we need to listen for?
 // What should happen when those event occur?
+
+// Read user input
+const userInput = document.querySelector("#input-box");
+const addButton = document.querySelector("#add-task-btn");
+
+addButton.addEventListener("click", () => {
+  // 1. Read text input
+  console.log("Button clicked");
+  const title = userInput.value;
+
+  // 2. Validate input
+  if (title === "") {
+    // If the input is empty return
+    alert("Please write something.");
+  }
+
+  // 3. Create new todo
+  else addTask(title);
+
+  // 4. Update the DOM
+  updateDisplay();
+
+  // 5. Clear the input field
+  userInput.value = "";
+});
+
+// Updating the DOM
+const listContainer = document.querySelector("#display-task");
+
+/**
+ * A function for updating the DOM to reflect tasks - this function is used in addButton.addEventListener
+ */
+function updateDisplay() {
+  // Get the updated list of all todoes
+  const allTodoes = getAllTodoes();
+
+  // Clear out the previous elements
+  listContainer.innerHTML = "";
+
+  // Go trough the list of all todoes and
+  for (const data of allTodoes) {
+    // Create a new HTML element
+    const newElement = createNewTask(data);
+    // Insert the new element into the DOM
+    listContainer.appendChild(newElement);
+  }
+}
--- scripts/todoList.js
+++ scripts/todoList.js
@@ -3,7 +3,6 @@
  *
  * @returns {Todo}
  */
-export function getAllTodoes() {}
 
 /**
  * Creates a new Todo based on the passed
@@ -12,4 +11,46 @@ export function getAllTodoes() {}
  * @param {TodoCreationInfo} data
  * @returns {Todo}
  */
-export function addTodo(data) {}
+
+/*Collection of all of variables and functions associated with Todo List*/
+
+// The list of all of our Todoes
+const todoes = [];
+
+// A function for getting all of Todoes
+export function getAllTodoes() {
+  return todoes;
+}
+
+// I creating a function for adding a new todo item (task) --> data from createTaskFunction
+// I have to create this function for write which things in data will be changed + adding max-length
+export function addTask(title) {
+  // Create our new Todo object
+  const newTask = {
+    id: createUniqueId(),
+    title: title,
+    LastUpdatedAt: new Intl.RelativeTimeFormat("en"),
+  };
+
+  // todoes.push(newTask);
+
+  // A function for regulation quantity of items in the list
+  if (todoes.length < 5) {
+    todoes.push(newTask);
+    console.log("Todo added:", newTask);
+  } else {
+    alert("You can not add more tasks. Please delete some of existed ones.");
+    console.log(
+      "You can not add more tasks. Please delete some of existed ones."
+    );
+  }
+}
+
+// Creating id of task
+let currentId = 0;
+
+function createUniqueId() {
+  const newId = currentId;
+  currentId++;
+  return newId;
+}
